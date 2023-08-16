fd4ac16249fb69766fcd6fddcd06b8d25e452d63
Lennart <hoff.lenn@gmail.com>
2023-08-16 17:01:21 +0200

    moved creeps into own file

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"CodeColony/units"
 	"fmt"
-	"math"
 )
 
 // we got a map that is build as a coordinate system.
+
 // we got a main building that can produce creeps
 
 // we also got creeps that can move, collect and carry
@@ -26,43 +27,7 @@ func (e EnergySource) getPosition() [2]int {
 }
 
 // should the creep also have a orientation? thatwould mean it can move only forward and has to turn.
-type Creep struct {
-	id   int
-	position [2]int
-}
 
-func (c *Creep) moveXCoordinate(move int){
-	c.position[0] = c.position[0] + move
-}
-
-func (c *Creep) moveYCoordinate(move int){
-	c.position[1] = c.position[1] + move 
-}
-
-func (c *Creep) moveTowardsEnergySource(e EnergySource) bool{
-// make it so that the creep will not move once it is directly infront of the source
-	destination := e.getPosition()
-	ex,ey := destination[0], destination[1]
-	cx,cy := c.position[0], c.position[1]
-	dx := ex - cx
-	dy := ey - cy
-	if math.Abs(float64(dx)) == 1 && math.Abs(float64(dy)) == 1 {
-		return true
-	}
-	move := func (dist int) int {
-		if dist >= 0 {
-			return 1
-		}
-		return -1
-	}
-
-	if math.Abs(float64(dx)) > math.Abs(float64(dy)) { // there is probably some edgecase here.
-		c.moveXCoordinate(move(dx))	
-	} else {
-		c.moveYCoordinate(move(dy))
-	}
-	return false
-}
 
 
 
@@ -71,15 +36,12 @@ func main()  {
 		id: 1,
 		position: [2]int{10, -32},
 	}
-
-	creep := Creep {
-		id : 1,
-		position: [2]int{0,0},
-	} 
+	c := creep.NewCreep([2]int{0,0})	
 	// this is my game engine
+	fmt.Println(c.GetName())
 	for i := 0; i < 100; i++ {
-		fmt.Println(creep.moveTowardsEnergySource(energySource))
-		fmt.Println(creep.position)
+		fmt.Println(c.MoveTowardsPoint(energySource.getPosition()))
+		fmt.Println(c.GetPosition())
 	}
 	
 }
---
+++ units/creep.go
@@ -0,0 +1,64 @@
+package creep
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+	type creep struct {
+		name   string
+		position [2]int
+	}
+
+	func NewCreep(position [2]int) creep{
+	c := creep {
+		name: fmt.Sprintf( "creep %d", time.Now().Minute()),
+		position: position,
+		}
+	return c
+	
+	}
+	func (c *creep) moveXCoordinate(move int){
+		c.position[0] = c.position[0] + move
+	}
+
+	func (c *creep) moveYCoordinate(move int){
+		c.position[1] = c.position[1] + move 
+	}
+	
+	func (c creep) GetName() string{
+		return c.name
+	}
+	
+	func (c creep) GetPosition() [2]int{
+		return c.position
+	}
+	//func (c Creep) checkIfNextToEnergySource(e []EnergySource) bool {
+	//for _, v := range e {
+	//		
+	//	}	
+	//}
+
+	func (c *creep) MoveTowardsPoint(destination [2]int) bool{
+	// make it so that the creep will not move once it is directly infront of the source
+		ex,ey := destination[0], destination[1]
+		cx,cy := c.position[0], c.position[1]
+		dx := ex - cx
+		dy := ey - cy
+		if math.Abs(float64(dx)) == 1 && math.Abs(float64(dy)) == 1 {
+			return true
+		}
+		move := func (dist int) int {
+			if dist >= 0 {
+				return 1
+			}
+			return -1
+		}
+
+		if math.Abs(float64(dx)) > math.Abs(float64(dy)) { // there is probably some edgecase here.
+			c.moveXCoordinate(move(dx))	
+		} else {
+			c.moveYCoordinate(move(dy))
+	}
+	return false
+}
