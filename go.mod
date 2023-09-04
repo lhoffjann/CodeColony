e3dde7326f0e894730d3d188dfcc4392e66895f8
Lennart <hoff.lenn@gmail.com>
2023-09-04 08:18:04 +0200

    first steps into hmtx

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module CodeColony
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"CodeColony/internal"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // we got a map that is build as a coordinate system.
@@ -13,6 +17,38 @@ import (
 // also there should definitly be more than one creep
 
 func main() {
+	go runGame()
+	go runHttpListener()
+	select {}
+}
+func runGame() {
 	g := internal.NewGame(40, 30)
 	g.Tick()
+}
+
+func runHttpListener() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", IndexHandler)
+
+	http.Handle("/", r)
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := `
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>htmx Example</title>
+		<script src="https://cdn.jsdelivr.net/npm/htmx.org@1.7.1/dist/htmx.min.js"></script>
+	</head>
+	<body>
+		<h1>htmx Example</h1>
+		<button hx-get="/data" hx-trigger="click" hx-target="#result">Load Data</button>
+		<div id="result"></div>
+	</body>
+	</html>
+	`
+
+	t, _ := template.New("index").Parse(tmpl)
+	t.Execute(w, nil)
 }
